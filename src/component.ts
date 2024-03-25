0b395d4054da2ecc8c17c7f99543d5449ecf690d
beanziii <93879641+beanziii@users.noreply.github.com>
2024-03-25 14:28:45 +0000

    accept multiple children

--- src/component.ts
+++ src/component.ts
@@ -1,43 +1,62 @@
-export class Component {
-  type: keyof HTMLElementTagNameMap;
-  parent: HTMLElement | null;
+type TChildren = HTMLElement[] | HTMLElement | string[] | string | null;
 
-  constructor(type: keyof HTMLElementTagNameMap, parent: HTMLElement | null) {
+export class Component {
+  private type: keyof HTMLElementTagNameMap;
+  private parent: HTMLElement | null;
+  private classNames: string[] | null = null;
+
+  constructor(
+    type: keyof HTMLElementTagNameMap,
+    parent: HTMLElement | null,
+    classNames: string[] | null = null
+  ) {
     this.type = type;
     this.parent = parent;
+    this.classNames = classNames;
   }
 
-  render({ children }: { children: HTMLElement | string | null }) {
+  render({ children }: { children: TChildren }) {
     const createdElement = document.createElement(this.type);
+    this.applyClassNames(createdElement);
+    this.appendToParent(createdElement);
 
-    if (!this.parent) {
-      if (!children) {
-        return createdElement;
-      }
-
-      if (typeof children === "string") {
-        const textNode = document.createTextNode(children);
-        createdElement.appendChild(textNode);
-        return createdElement;
-      }
-
-      createdElement.appendChild(children);
-      return createdElement;
+    if (children) {
+      this.handleChildren(createdElement, children);
     }
 
-    this.parent.appendChild(createdElement);
-
-    if (!children) {
-      return createdElement;
-    }
-
-    if (typeof children === "string") {
-      const textNode = document.createTextNode(children);
-      createdElement.appendChild(textNode);
-      return createdElement;
-    }
-
-    createdElement.appendChild(children);
     return createdElement;
+  }
+
+  private applyClassNames(element: HTMLElement) {
+    if (this.classNames) {
+      this.classNames.forEach((className) => element.classList.add(className));
+    }
+  }
+
+  private appendToParent(element: HTMLElement) {
+    if (this.parent) {
+      this.parent.appendChild(element);
+    }
+  }
+
+  private handleChildren(element: HTMLElement, children: TChildren) {
+    if (Array.isArray(children)) {
+      children.forEach((child) => {
+        if (child !== null) {
+          this.appendChild(element, child);
+        }
+      });
+    } else if (children !== null) {
+      this.appendChild(element, children);
+    }
+  }
+
+  private appendChild(element: HTMLElement, child: HTMLElement | string) {
+    if (typeof child === "string") {
+      const textNode = document.createTextNode(child);
+      element.appendChild(textNode);
+    } else {
+      element.appendChild(child);
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,23 @@ import { Component } from "./component.ts";
 
 const root = document.getElementById("root")!;
 
-const header = new Component("header", root).render({
-  children: new Component("h1", null).render({ children: "My title!!!" }),
+const Title = new Component("h1", null, ["title"]).render({
+  children: "My site",
 });
 
-new Component("button", header).render({ children: "Click me" });
+const SubTitle = new Component("p", null, ["sub-title"]).render({
+  children:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+});
+
+new Component("section", root, ["hero", "is-primary"]).render({
+  children: new Component("div", null, ["hero-body"]).render({
+    children: [Title, SubTitle],
+  }),
+});
+
+new Component("main", root, ["section"]).render({
+  children: new Component("button", null, ["button"]).render({
+    children: "Click me!",
+  }),
+});
